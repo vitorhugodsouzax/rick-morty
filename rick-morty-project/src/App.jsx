244cf488b78b937cc00a896094101d2baa1215b6
Vitor hugo <vitorhugodesouzaph@gmail.com>
2023-10-16 19:43:19 -0300

    pronto

--- rick-morty-project/src/App.jsx
+++ rick-morty-project/src/App.jsx
@@ -31,38 +31,58 @@ function App() {
 
   // Estado e lógica relacionados aos detalhes do personagem
   const [selectedCharacter, setSelectedCharacter] = useState(null);
+  const [episodes, setEpisodes] = useState([]);
 
   const showDetails = async (characterId) => {
     try {
       const response = await fetch(`https://rickandmortyapi.com/api/character/${characterId}`);
       const data = await response.json();
       setSelectedCharacter(data);
+      fetchForEachEpisode(data);
     } catch (error) {
       console.error('Error fetching character details:', error);
     }
+  };
+
+  const fetchForEachEpisode = async (character) => {
+    const allEpisodes = [];
+
+    for (let i = 0; i < character.episode.length; i++) {
+      const url = character.episode[i];
+      const responseEpisodes = await fetch(url);
+      const dataEpisodes = await responseEpisodes.json();
+      allEpisodes.push(dataEpisodes);
+    }
+
+    setEpisodes(allEpisodes);
   };
 
   return (
     <div className="app-container">
       <div className="character-list">
         <h2>Personagens de Rick and Morty</h2>
-        <input
-          type="text"
-          placeholder="Pesquisar por personagem"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <select value={filterStatus} onChange={(e) => setFilterStatus(e.target.value)}>
-          <option value="">Todos os Status</option>
-          <option value="alive">Vivo</option>
-          <option value="dead">Morto</option>
-          <option value="unknown">Desconhecido</option>
-        </select>
+        <div className="pesquisa-container">
+          <input
+            type="text"
+            placeholder="Pesquisar por personagem"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="pesquisa"
+          />
+          <select value={filterStatus} onChange={(e) => setFilterStatus(e.target.value)} className="pesquisa">
+            <option value="">Todos os Status</option>
+            <option value="alive">Vivo</option>
+            <option value="dead">Morto</option>
+            <option value="unknown">Desconhecido</option>
+          </select>
+        </div>
         <ul>
           {characters.map((character) => (
             <li key={character.id} onClick={() => showDetails(character.id)} className="personagem-item">
-              <img className="imagem-personagem" src={character.image} alt={`${character.name} thumbnail`} />
-              <span className="nome-personagem">{character.name}</span>
+              <div className="character-info">
+                <img className="imagem-personagem" src={character.image} alt={`${character.name} thumbnail`} />
+                <span className="nome-personagem">{character.name}</span>
+              </div>
             </li>
           ))}
         </ul>
@@ -71,7 +91,7 @@ function App() {
         <div className="character-details">
           <div className="detailsContainer">
             <div className="imageContainer">
-              <div className="detail-title">Detalhes do Personagem</div>
+              <h3 className="detail-title">Detalhes do Personagem</h3>
               <img
                 src={selectedCharacter.image}
                 alt={`${selectedCharacter.name} thumbnail`}
@@ -83,22 +103,13 @@ function App() {
               <p><strong>Nome:</strong> {selectedCharacter.name}</p>
               <p><strong>Status:</strong> {selectedCharacter.status}</p>
               <p><strong>Espécie:</strong> {selectedCharacter.species}</p>
-              <h3>Episódios:</h3>
+              <h1 className="title-episodio">Episódios</h1>
               <ul>
-              {selectedCharacter && selectedCharacter.episode && selectedCharacter.episode.map((episode, index) => {
-  const parts = episode.split('/');
-  const episodeNumber = parts[parts.length - 1];
-  const episodeTitle = selectedCharacter.episodeTitle && selectedCharacter.episodeTitle[index];
-  const titleText = episodeTitle ? `Título: ${episodeTitle}` : 'Título desconhecido';
-  return (
-    <li key={index}>
-      Número: {episodeNumber}, {titleText}
-    </li>
-  );
-})}
-
-  
-
+                {episodes.map((episode) => (
+                  <li key={episode.id} className="li">
+                    <h1 className="episodios">{episode.episode}, {episode.name}</h1>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
